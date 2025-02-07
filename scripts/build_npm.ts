4e5efcfdac6c0d4a00e973a26d6d77a2c313f089
kennycallado <kennycallado@hotmail.com>
2025-02-07 20:21:09 +0100

    ADD(scripts)
    build_npm: build the package for npm

---
+++ scripts/build_npm.ts
@@ -0,0 +1,40 @@
+import { build, emptyDir } from "jsr:@deno/dnt";
+import denoPkg from "../deno.json" with { type: "json" };
+
+await emptyDir("./npm");
+
+await build({
+	entryPoints: ["./mod.ts"],
+	outDir: "./npm",
+
+	shims: {
+		// see JS docs for overview and more options
+		deno: true,
+	},
+
+	typeCheck: "both",
+	compilerOptions: {
+		lib: ["DOM"],
+	},
+
+	package: {
+		// package.json properties
+		name: denoPkg.name,
+		version: denoPkg.version,
+		description: "Your package.",
+		license: denoPkg.license,
+		// repository: {
+		// 	type: "git",
+		// 	url: "git+https://github.com/username/repo.git",
+		// },
+		// bugs: {
+		// 	url: "https://github.com/username/repo/issues",
+		// },
+	},
+
+	postBuild() {
+		// steps to run after building and before running the tests
+		// Deno.copyFileSync("LICENSE", "npm/LICENSE");
+		Deno.copyFileSync("README.md", "npm/README.md");
+	},
+});
