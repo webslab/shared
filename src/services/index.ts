51b4f4175413d34f70dd94e5823e1107998ccae8
kennycallado <kennycallado@hotmail.com>
2025-02-09 11:02:04 +0100

    ADD(services)
    paper: user module outcomes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth/index.ts";
+export * from "./paper/index.ts";
 export * from "./theming/index.ts";
 export * from "./database/index.ts";
---
+++ src/services/paper/paper.service.ts
@@ -0,0 +1,120 @@
+import { RecordId } from "surrealdb";
+
+import type { AuthService } from "../auth/auth.service.ts";
+import type { Module, Paper, Question } from "../../types/index.ts";
+
+type Answer = {
+	content: string;
+	question: RecordId | Question;
+};
+
+type Action = {
+	name: "start" | "next" | "prev" | "finish";
+	current?: number;
+	time: Date;
+};
+
+export class PaperService implements Paper {
+	actions: Action[] = [];
+	answers: Answer[];
+
+	authSvc;
+	module;
+	user;
+
+	constructor(module: Module, authSvc: AuthService) {
+		this.authSvc = authSvc;
+		this.module = module;
+
+		this.user = this.authSvc.getUser()!;
+		this.answers = this.searchQuestions();
+
+		this.startPaper();
+	}
+
+	// throw "";
+	public async submit(current: number) {
+		if (this.answers.length !== 0) {
+			this.updateAnswers();
+		}
+
+		this.actions.push({
+			current,
+			name: "finish",
+			time: new Date(),
+		});
+
+		try {
+			await this.save();
+		} catch (e: unknown) {
+			console.log(e);
+		}
+		// return error message in case
+	}
+
+	public next(current: number) {
+		this.actions.push({
+			current,
+			name: "next",
+			time: new Date(),
+		});
+	}
+
+	public prev(current: number) {
+		this.actions.push({
+			current,
+			name: "prev",
+			time: new Date(),
+		});
+	}
+
+	private async save() {
+		const prepare: Paper = {
+			user: new RecordId("user", (this.user.id as string).split(":")[1]),
+			// user: new StringRecordId(this.user.id!),
+			module: this.module.id as RecordId,
+			answers: this.answers,
+			actions: this.actions,
+		};
+		console.log(prepare);
+
+		try {
+			// can't use create because it needs permissions for select
+			// const res = await this.authSvc.getDb().create("paper", prepare);
+			await this.authSvc
+				.getDb()
+				.query(
+					`CREATE paper CONTENT { user: $user, module: $module, answers: $answers, actions: $actions };`,
+					prepare,
+				);
+		} catch (e: unknown) {
+			throw e;
+		}
+	}
+
+	private updateAnswers() {}
+
+	private startPaper(): void {
+		this.actions.push({
+			name: "start",
+			time: new Date(),
+		});
+	}
+
+	private searchQuestions(): Answer[] {
+		return this.module
+			.content
+			.filter((content) => content.includes("<wl-question"))
+			.map((content) => {
+				const qid = content.match(/<wl-question.*qid="question:(.*)".*>/);
+
+				if (qid) {
+					return {
+						content: "",
+						question: new RecordId("question", qid[1]),
+					};
+				}
+			})
+			.filter((question) => question !== undefined);
+	}
+}
